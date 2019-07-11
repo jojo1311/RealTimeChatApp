085b70b0c7818cb0423722e66db1f3d0bfc2b37d
jojo1311 <josette@cocuyostudio.com>
2019-07-11 14:51:35 -0400

    added function app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-let channels = [
-        {name: 'Hardware Support'},
-        {name: 'Software Support'}
-];
+
 
 class Channel extends React.Component{
     onClick(){
@@ -31,6 +28,11 @@ class ChannelList extends React.Component{
 }
 
 class ChannelForm extends React.Component{
+    // Initialize state object
+    constructor(props){
+        super(props);
+        this.state = { };
+    }
     //Function to access input value.
     onChange(e){
         this.setState({
@@ -39,26 +41,52 @@ class ChannelForm extends React.Component{
        // console.log(e.target.value);
     }
     onSubmit(e){
-        let {channelName} = this.state;
-        console.log(channelName);
+        // clearing out input form
+        this.setState({
+            channelName: ' '
+        });
+        this.props.addChannel(channelName);
         e.preventDefault();
     }
     render(){
         return(
             //If we want to have access to react component within  an  event handler we need to  call bind and passed this.
             <form onSubmit={this.onSubmit.bind(this)}>
-                <input type='text'  placeholder="Add New Channel"  onChange={this.onChange.bind(this)} />
+                <input type='text'  placeholder="Add New Channel"
+                  onChange={this.onChange.bind(this)} 
+                  value={this.state.channelName}
+                  />
            </form>
         )
     }
 }
 // create component section -->
 class ChannelSection extends React.Component {
+    // initialize state object to passed array
+    constructor(props){
+        super (props);
+        this.state = {
+            channels: [
+                {name: 'Hardware Support'},
+                {name: 'Software Support'}
+            ]
+        }
+    }
+    //create and addChannel() function and pass it to Channel form as a property
+    addChannel(name){
+        //variable to corresponding values set in state
+        let {channels} = this.state;
+        channels.push({name: name});
+        this.setState({
+            channels: channels
+        })
+
+    }
     render(){
         return(
             <div>
-                <ChannelList channels={channels}/>
-                <ChannelForm/>
+                <ChannelList channels={this.state.channels}/>
+                <ChannelForm addChannel={this.addChannel.bind(this)}/>
             </div>
         )
     }
